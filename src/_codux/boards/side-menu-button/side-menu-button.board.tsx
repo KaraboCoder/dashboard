5ba694566697ca2f9c04593f250633c6cac0a73b
Karabo Ngwato <karabo@linumlabs.com>
2023-01-21 01:08:54 +0200

    fixed build issues

--- src/_codux/boards/side-menu-button/side-menu-button.board.tsx
+++ src/_codux/boards/side-menu-button/side-menu-button.board.tsx
@@ -1,7 +1,10 @@
 import { createBoard } from '@wixc3/react-board';
 import { SideMenuButton } from '../../../components/side-menu-button/side-menu-button';
-
+const props = {
+    icon: 'menu',
+    text: 'Dashboard',
+};
 export default createBoard({
     name: 'SideMenuButton',
-    Board: () => <SideMenuButton />
+    Board: () => <SideMenuButton {...props} />,
 });
